195a324fe98851aeee62eb3b4d2749d0ba39d4ee
Pifkó Gergely József <V5I5CL@uni-mate.hu>
2026-03-07 09:22:16 +0100

    Created viragok

---
+++ C#-2/02/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+
+class Viragcsokor
+{
+    private string nev { get; set; }
+    private int szal { get; set; }
+    public Viragcsokor(string nev, int szal)
+    {
+        nev = nev;
+        szal = szal;
+    }
+
+    public override string ToString()
+    {
+        return $"{nev} {szal}";
+    }
+
+    public static bool operator >(Viragcsokor v1, Viragcsokor v2)
+    {
+        return v1.szal > v2.szal;
+    }
+
+    public static bool operator <(Viragcsokor v1, Viragcsokor v2)
+    {
+        return v1.szal < v2.szal;
+    }
+
+    public static bool operator ==(Viragcsokor v1, Viragcsokor v2)
+    {
+        return v1.szal == v2.szal;
+    }
+
+    public static bool operator !=(Viragcsokor v1, Viragcsokor v2)
+    {
+        return v1.szal != v2.szal;
+    }
+
+    public static implicit operator int(Viragcsokor v)
+    {
+        return v.szal;
+    }
+
+    public static explicit operator double(Viragcsokor v)
+    {
+        return v.szal;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Viragcsokor v1 = new Viragcsokor("Tulipán", 11);
+        Viragcsokor v2 = new Viragcsokor("Rózsa", 5);
+       
+        Console.WriteLine(v1 < v2);
+
+    }
+}
