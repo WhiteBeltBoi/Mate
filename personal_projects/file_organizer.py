3ee1b747665582340094b79e846082a3a9dd8eff
WhiteBeltBoi <gergelypifko@gmail.com>
2026-03-18 21:42:55 +0100

    made a very simple little file organizer

---
+++ personal_projects/file_organizer.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import shutil
+
+source_dir = Path("/Users/johndoe/Downloads")
+text_destination = Path("/Users/johndoe/Documents/text_files")
+picture_destination = Path("/Users/johndoe/Pictures/picture_collection")
+counter = 0
+
+
+for txt_file in source_dir.glob("*.txt"):
+    shutil.move(str(txt_file), str(text_destination/txt_file.name))
+    counter += 1
+
+for pattern in ("*.jpg", "*.png","*.PNG", "*.HEIC","*.heic", "*.CR3", "JPEG"):
+    for picture_file in source_dir.glob(pattern):
+        shutil.move(str(picture_file), str(picture_destination/picture_file.name))
+        counter += 1
+
+print(f"Done, moved {counter} files")
