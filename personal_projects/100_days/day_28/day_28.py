10ab76975b3fde3d8e187678af8d0dd0ef1d1af4
WhiteBeltBoi <gergelypifko@gmail.com>
2026-03-24 13:47:51 +0100

    Done an other dict comprehension exercise

--- personal_projects/100_days/day_28/day_28.py
+++ personal_projects/100_days/day_28/day_28.py
@@ -10,3 +10,8 @@ passed_students = {}
 
 passed_students_comp= {student:score for student, score in student_scores.items() if score >= 60}
 print(passed_students_comp)
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# list_sentence = [word for word in sentence.split("")]
+result = {word:len(word) for word in sentence.split()}
+print(result)
